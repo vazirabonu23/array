6ee9d1cd848046fbb6833d0a83b3a57b0b9c985c
vazirabonu [FE16] <102415721+vazirabonu23@users.noreply.github.com>
2022-12-19 15:07:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let arr = [1, 2, 3, 4, 5 ];
+let pop = arr.length - 1;
+
+arr.splice(pop);
+
+console.log(arr); 
+
+
+
+let arr1 = [ "NewYork", "UK","Russia"];
+arr1.splice(3, 0, "USA")
+console.log(arr1);
